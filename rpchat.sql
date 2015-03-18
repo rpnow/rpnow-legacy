198c62f84bdb2c72f842e0e12f7d9c73f0d9e261
shamblesides <nigel.mark.nelson@gmail.com>
2015-03-18 03:11:18 -0600

    database structure dump

---
+++ rpchat.sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2015 at 10:08 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `rpchat`
+--
+CREATE DATABASE IF NOT EXISTS `rpchat` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `rpchat`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `character`
+--
+
+CREATE TABLE IF NOT EXISTS `character` (
+  `Name` varchar(30) NOT NULL,
+  `Color` tinytext NOT NULL,
+  `Room` char(4) NOT NULL,
+  PRIMARY KEY (`Name`,`Room`),
+  KEY `Room` (`Room`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE IF NOT EXISTS `message` (
+  `Content` text NOT NULL,
+  `Is_Action` tinyint(1) NOT NULL DEFAULT '0',
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Character_Name` varchar(30) NOT NULL,
+  `Character_Room` char(4) NOT NULL,
+  `Number` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Number`),
+  KEY `Timestamp` (`Timestamp`),
+  KEY `Character` (`Character_Name`,`Character_Room`),
+  KEY `Character_Room` (`Character_Room`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE IF NOT EXISTS `room` (
+  `ID` char(4) NOT NULL,
+  `Title` tinytext NOT NULL,
+  `Description` tinytext NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `character`
+--
+ALTER TABLE `character`
+  ADD CONSTRAINT `character_ibfk_1` FOREIGN KEY (`Room`) REFERENCES `room` (`ID`);
+
+--
+-- Constraints for table `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `Character` FOREIGN KEY (`Character_Name`, `Character_Room`) REFERENCES `character` (`Name`, `Room`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
