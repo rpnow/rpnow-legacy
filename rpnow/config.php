42cd55f4e2acb48f2a37a6e12149138cee83d552
shamblesides <nigel.mark.nelson@gmail.com>
2015-09-30 01:06:03 -0600

    improved and updated comments in default config.php

--- rpnow/config.php
+++ rpnow/config.php
@@ -1,24 +1,31 @@
 <?php
-# -----
-# Default RP Server configuration file.
-# You'll need to change some of these values before
-# your RP Server works correctly. The root path and the
-# database settings, specifically.
-#
-# Configuration variables follow.
-# -----
+
+##-----
+##
+## Default RPNow configuration file.
+##
+## You MUST provide database credentials in order for the
+## server to work!
+##
+## Other default configuration settings work out-of-the-box,
+## but can be modified if you wish. This includes things such
+## as enabling the admin panel, changing the length of the
+## URLs for each RP, and displaying a broadcast message to all
+## users on the site.
+##
+##-----
 
 ## The server on which the database is located;
-## if it's one the same server, use 'localhost'
+##  If it's the same as the webserver, use 'localhost'.
 $rpDBServer = 'localhost';
 ## Database name
 $rpDBName   = 'rp_db';
-## The user that has permissions to the database
+## A user with SELECT and INSERT permissions to that database.
 $rpDBUser   = 'rp_db_user';
 ## Password for that user; PLEASE make it long and random!
 $rpDBPass   = 'abc123';
 
-## Is the admin panel enabled?
+## Uncomment the following line to enable the admin panel.
 # $rpAdminPanelEnabled = true;
 ## Admin panel username and password.
 ## Uncomment and ***CHANGE*** these when you enable the panel!
@@ -26,30 +33,33 @@ $rpDBPass   = 'abc123';
 # $rpAdminPanelPass = 'admin';
 
 ## The length of the auto-generated URL for each RP room.
-## Minimum recommended for a live website is 4.
-## Maximum allowed is 20.
+##  Minimum recommended for a live website is 4.
+##  Maximum allowed is 20.
 $rpIDLength = 7;
 ## The characters to be used in that URL.
-## By default ambiguous characters (such as O and 0) are excluded.
+##  By default, certain ambiguous characters (such as O and 0)
+##  are excluded entirely.
 $rpIDChars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789';
-## How many posts should show up on the main RP page.
-## Also how many should be on each page of the archive.
+## How many posts long should an archive page be?
+##  Also determines how many messages are retained on the
+##  chat page simultaneously
 $rpPostsPerPage = 20;
-## How often (in milliseconds) the page should check the
-## server for updates.
+## How often (in milliseconds) the chat page should check the
+##  server for updates.
 $rpRefreshMillis = 3000;
 
 ## Admin Broadcast Message
-## When you need to deliver a message to the entire site,
-## uncomment the following line and set the message text to
-## what you want it to say.
-## HTML works.
+##  When you need to deliver a message to the entire site,
+##  uncomment the following line and set the message text to
+##  what you want it to say.
+##  HTML works.
 # $rpBroadcast = "<h3>Admin Broadcast</h3><p>This is your captain speaking.</p>";
+
 ## Maintenance Message
-## When taking the site down for maintenance, uncomment the
-## following line and change the message.
-## Once maintenance is done, just comment it out again!
-## HTML is allowed, again.
-# $rpDown = "<p>Sorry! RPNow is down for routine maintenance.</p>";
+##  When taking the site down for maintenance, uncomment the
+##  following line and change the message.
+##  Once maintenance is done, just comment it out again!
+##  HTML is allowed, again.
+# $rpDown = "<p>Temporarily down for maintenance; sorry!</p>";
 
 ?>
