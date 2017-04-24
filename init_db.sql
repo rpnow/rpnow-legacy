27acfdf54df30e6e8e0ccaa3473c9e2b4d334df7
Nigel Nelson <nigel.mark.nelson@gmail.com>
2017-04-24 17:12:15 -0600

    Finally, initialize database upon creation

--- init_db.sql
+++ init_db.sql
@@ -1,13 +1,7 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `Character`
 --
 
-DROP TABLE IF EXISTS `Character`;
 CREATE TABLE IF NOT EXISTS `Character` (
   `Number` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(30) NOT NULL,
@@ -24,26 +18,9 @@ CREATE TABLE IF NOT EXISTS `Character` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Chara_Update`
---
-
-DROP TABLE IF EXISTS `Chara_Update`;
-CREATE TABLE IF NOT EXISTS `Chara_Update` (
-  `Number` int(11) NOT NULL AUTO_INCREMENT,
-  `Chara_Number` int(11) NOT NULL,
-  `Action` enum('delete','undelete') NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `IP` varchar(45) NOT NULL,
-  PRIMARY KEY (`Number`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `Message`
 --
 
-DROP TABLE IF EXISTS `Message`;
 CREATE TABLE IF NOT EXISTS `Message` (
   `Number` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `Type` enum('Narrator','Character','OOC') NOT NULL,
@@ -62,26 +39,9 @@ CREATE TABLE IF NOT EXISTS `Message` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Message_update`
---
-
-DROP TABLE IF EXISTS `Message_Update`;
-CREATE TABLE IF NOT EXISTS `Message_Update` (
-  `Number` int(11) NOT NULL AUTO_INCREMENT,
-  `Message_Number` int(11) NOT NULL,
-  `Action` enum('delete','undelete') NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `IP` varchar(45) NOT NULL,
-  PRIMARY KEY (`Number`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `Room`
 --
 
-DROP TABLE IF EXISTS `Room`;
 CREATE TABLE IF NOT EXISTS `Room` (
   `Number` int(11) NOT NULL AUTO_INCREMENT,
   `ID` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
